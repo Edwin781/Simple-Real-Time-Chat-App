316d2177524133d9128bdbd31527e0261392599f
Edwin Nnorom <nnoromedwin@gmail.com>
2020-08-06 23:56:47 -0500

    Made changes to logger

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
-
-var express = require('express');
-var Socket = require('socket.io');
+var express = require("express");
+var Socket = require("socket.io");
 
 //App Setup
 var App = express();
 
-
-var server = App.listen(8080,function(){
-
-    console.log('Listening on port 8080');
-})
+var server = App.listen(8080, function () {
+  console.log("Listening on port 8080");
+});
 
 //static file
-App.use(express.static('Public'));
+App.use(express.static("Public"));
 
 //Socket Setup
 //We want socket.io to work on this socket and listen for connection
@@ -21,30 +18,25 @@ var io = Socket(server);
 var username;
 
 //listen for connection
-io.on('connection', function(socket){
-    console.log('New User Connected', socket.id);
+io.on("connection", function (socket) {
+  console.log("Socket info", socket.handshake.address);
+  console.log("New User Connected", socket.id);
 
-    socket.on('chat',function(data){
-        username = data.handle;
-        io.sockets.emit('chat',data);
-    });
+  socket.on("chat", function (data) {
+    username = data.handle;
+    io.sockets.emit("chat", data);
+  });
 
-    socket.on('Username', function(data){
-        
-        /* var username = socket.data;
+  socket.on("Username", function (data) {
+    /* var username = socket.data;
         socket.emit('CurrentUser',username) */
-    });
+  });
 
-    socket.on('disconnect', function(){
-        
-       socket.broadcast.emit('user-left', username);
-        
-    });
-   
-    socket.on('typing', function(data){
-    socket.broadcast.emit('typing', data);
-    });
+  socket.on("disconnect", function () {
+    socket.broadcast.emit("user-left", username);
+  });
+
+  socket.on("typing", function (data) {
+    socket.broadcast.emit("typing", data);
+  });
 });
-
-
-
