c4e2e8701a7533facca36a67fe849f2181b741bc
Edwin Nnorom <nnoromedwin@gmail.com>
2020-08-07 02:40:17 -0500

    test

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 var express = require("express");
 var app = express();
 var path = require("path");
+const { getHeapCodeStatistics } = require("v8");
 var server = require("http").createServer(app);
 var io = require("socket.io")(server);
 var port = process.env.PORT || 3000;
 
 var ip;
-// Routing
-//app.use(express.static(path.join(__dirname, "public")));
+
+//Routing
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
-  var ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
   if (ip.substr(0, 7) == "::ffff:") {
     ip = ip.substr(7);
   }
@@ -19,9 +21,14 @@ app.get("/", function (req, res) {
 
 server.listen(port, (data) => {
   console.log("Server listening at port %d", port);
-  console.log("Some data", data);
 });
 
+getmydata = (ip) => {
+  if (ip.substr(0, 7) == "::ffff:") {
+    return (ip = ip.substr(7));
+  }
+  return ip;
+};
 //Socket Setup
 //We want socket.io to work on this socket and listen for connection
 
@@ -29,31 +36,35 @@ server.listen(port, (data) => {
 var username;
 
 //listen for connection
-// io.on("connection", function (socket) {
-//   ip = socket.handshake.address;
-//   console.log("Socket info", socket.handshake.address);
-//   console.log("New User Connected", socket.id);
+io.on("connection", function (socket) {
+  ip = getmydata(socket.handshake.address);
+  //   console.log("this is it", ip);
+  //   console.log("Socket info", socket.handshake.address);
+  console.log("New User Connected", socket.id);
 
-//   io.sockets.emit("logger", {
-//     username,
-//     ip,
-//   });
+  socket.on("chat", function (data) {
+    username = data.handle;
+    io.sockets.emit("chat", data);
+  });
 
-//   socket.on("chat", function (data) {
-//     username = data.handle;
-//     io.sockets.emit("chat", data);
-//   });
+  socket.on("Username", function (data) {
+    /* var username = socket.data;
+         socket.emit('CurrentUser',username) */
 
-//   socket.on("Username", function (data) {
-//     /* var username = socket.data;
-//         socket.emit('CurrentUser',username) */
-//   });
+    var username = data.handle;
 
-//   socket.on("disconnect", function () {
-//     socket.broadcast.emit("user-left", username);
-//   });
+    console.log(`Current user detail is ${username} and Ip -> ${ip}`);
+    /* io.sockets.emit("logger", {
+      username,
+      ip,
+    }); */
+  });
 
-//   socket.on("typing", function (data) {
-//     socket.broadcast.emit("typing", data);
-//   });
-// });
+  socket.on("disconnect", function () {
+    socket.broadcast.emit("user-left", username);
+  });
+
+  socket.on("typing", function (data) {
+    socket.broadcast.emit("typing", data);
+  });
+});
